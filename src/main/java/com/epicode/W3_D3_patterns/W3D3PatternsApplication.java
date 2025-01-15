8b5b1eacbf2ce5f50102e0d5dd011254100bfd30
Ikadev95 <fegughi@gmail.com>
2025-01-15 14:24:13 +0100

    Initial commit

---
+++ src/main/java/com/epicode/W3_D3_patterns/W3D3PatternsApplication.java
@@ -0,0 +1,13 @@
+package com.epicode.W3_D3_patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3D3PatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(W3D3PatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicode/W3_D3_patterns/W3D3PatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicode.W3_D3_patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3D3PatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
